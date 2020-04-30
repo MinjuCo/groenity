4cffd9095ce72a8ce913bd892f3c483cd7200bbe
brovnie <maarl.deenis@gmail.com>
2020-04-30 16:23:57 +0200

    footer done

---
+++ includes/nav.inc.php
@@ -0,0 +1,6 @@
+<nav>
+<div>
+<a href="index.php"><img src="img/groenity-logo.svg" alt="logo-groenity"></a>
+
+</div>
+</nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="nl">
+<?php include_once("includes/head.inc.php"); ?> 
+<body>
+    <?php include_once("includes/nav.inc.php"); ?>
+    Here comes the header
+    <?php include_once("includes/footer.inc.php"); ?>
+</body>
+</html>
