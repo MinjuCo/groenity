32238407b45147a2cd617e172c6898fc8dae4211
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-01 02:01:56 +0200

    Fixed the layout of the navigation bar with bootstrap classes and made head.inc.php more flexible for all pages

--- includes/nav-public.inc.php
+++ includes/nav-public.inc.php
@@ -1,16 +1,14 @@
- <div class="collapse navbar-collapse text-right" id="myNavbar7">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a href="#" class="nav-link">Over ons</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Live kaart</a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">Contact</a>
-            </li>
-            <li class="nav-item aanmelden-container">
-                <a href="#" class="nav-link aanmelden">Meld je aan</a>
-            </li>
-        </ul>
-    </div>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Over ons</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Live kaart</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Contact</a>
+        </li>
+        <li class="nav-item aanmelden">
+            <a href="#" class="nav-link aanmelden d-inline-block">Meld je aan</a>
+        </li>
+    </ul>
--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,20 +1,19 @@
-<nav class="navbar navbar-expand-lg static-top">
-    <div class="container-lg">
-        <a class="navbar-brand" href="index.php">
-            <img src="img/groenity-logo.svg" alt="logo-groenity">
-        </a>
-        <!--responsive button-->
-        </a>
+<nav class="navbar navbar-expand-md fixed-top d-flex flex-row align-items-center">
+    <div class="container-xl">
+    <a class="navbar-brand" href="<?php echo ($currentPage == "index")? "index.php": "../index.php";?>">
+        <img src="<?php echo ($currentPage == "index")? "img/": "../img/";?>groenity-logo.svg" alt="logo-groenity">
+    </a>
+    <!--responsive button-->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarResponsive">
             <?php
             if (isset($_SESSION["user"])) {
-                include_once('includes/nav-private.inc.php');
+                include_once(__DIR__."/../includes/nav-private.inc.php");
             } else {
-                include_once("includes/nav-public.inc.php");
+                include_once(__DIR__."/../includes/nav-public.inc.php");
             } ?>
-        </div>
+    </div>
     </div>
 </nav>
