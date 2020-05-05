190ba8cf8e0e7e7fac9c5f11994eea963a7ea2b3
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-05 10:58:27 +0200

    Example template of the page city

--- application/city.php
+++ application/city.php
@@ -13,8 +13,28 @@
     ?>
     
     <div class="container application">
-        <h2 class="mb-2">Stad</h2>
+        <div class="row d-flex flex-column">
+            <h2 class="mb-2">Stad</h2>
+            <ul class="nav nav-pills mb-1" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active rounded-pill" id="pills-city-tab" data-toggle="pill" href="#pills-city" role="tab" aria-controls="pills-city" aria-selected="true">Mijn stad</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false">Verkennen</a>
+                </li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="tab-content col-md-9 card shadow-sm" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-city" role="tabpanel" aria-labelledby="pills-city-tab">...</div>
+                <div class="tab-pane fade" id="pills-explore" role="tabpanel" aria-labelledby="pills-explore-tab">...</div>
+            </div>
+            <div class="col-md-3 card shadow-sm">
+                ...
+            </div>
+        </div>
     </div>
+    
     
     <?php 
         //Footer + scripts
