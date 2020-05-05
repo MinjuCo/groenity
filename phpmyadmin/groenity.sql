0fda536a27283f9fd469d950d6d9ec5780c77446
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-05-05 19:52:38 +0200

    Database updated, try to drop the old one and import this one

--- phpmyadmin/groenity.sql
+++ phpmyadmin/groenity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2020 at 01:20 PM
+-- Generation Time: May 05, 2020 at 07:50 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -28,18 +28,28 @@ SET time_zone = "+00:00";
 -- Table structure for table `challenges`
 --
 
-CREATE TABLE `challenges` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `challenges` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(300) NOT NULL,
-  `description` text NOT NULL,
+  `description` varchar(500) NOT NULL,
   `goals` varchar(300) NOT NULL,
   `extra_info` varchar(300) NOT NULL,
   `rewards` varchar(300) NOT NULL,
   `green_points` int(11) NOT NULL,
+  `thema_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `created_by` int(11) NOT NULL,
-  `is_battle` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `is_battle` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `challenges`
+--
+
+INSERT INTO `challenges` (`id`, `title`, `description`, `goals`, `extra_info`, `rewards`, `green_points`, `thema_id`, `created_at`, `created_by`, `is_battle`) VALUES
+(1, 'Energieverbruik verminderen', 'Het enorme verbruik van fossiele brandstoffen sinds de industriele revolutie ligt aan de basis van\r\nde opwarming van onze aarde. \r\nDeze opwarming wordt veroorzaakt door het broeikaseffect.', '1;2', 'links', '1;2', 300, 1, '2020-05-05 14:55:31', 2, 1),
+(2, 'Energieverbruik met 5% verminderen', 'Test description', '1;2', 'links', '1;2', 200, 1, '2020-05-05 14:57:31', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -47,16 +57,39 @@ CREATE TABLE `challenges` (
 -- Table structure for table `challenge_battles`
 --
 
-CREATE TABLE `challenge_battles` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `challenge_battles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `participator_one` int(11) NOT NULL,
   `participator_two` int(11) NOT NULL,
   `challenge_id` int(11) NOT NULL,
-  `winner_id` int(11) NOT NULL,
   `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `is_started` tinyint(1) NOT NULL,
-  `active` tinyint(1) NOT NULL
+  `is_completed` tinyint(1) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `challenge_battles`
+--
+
+INSERT INTO `challenge_battles` (`id`, `participator_one`, `participator_two`, `challenge_id`, `start_time`, `end_time`, `is_started`, `is_completed`, `active`) VALUES
+(1, 1, 2, 1, '2020-05-05 16:44:48', '0000-00-00 00:00:00', 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `challenge_completed`
+--
+
+CREATE TABLE IF NOT EXISTS `challenge_completed` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `challenge_id` int(11) NOT NULL,
+  `isWinner` tinyint(1) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -65,12 +98,13 @@ CREATE TABLE `challenge_battles` (
 -- Table structure for table `challenge_participators`
 --
 
-CREATE TABLE `challenge_participators` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `challenge_participators` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `challenge_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `active` tinyint(1) NOT NULL
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -79,13 +113,14 @@ CREATE TABLE `challenge_participators` (
 -- Table structure for table `cities`
 --
 
-CREATE TABLE `cities` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `zip` varchar(4) NOT NULL,
   `city` varchar(300) NOT NULL,
   `country` varchar(300) NOT NULL,
   `population` int(11) NOT NULL,
-  `green_points` int(11) NOT NULL
+  `green_points` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -94,11 +129,42 @@ CREATE TABLE `cities` (
 -- Table structure for table `role`
 --
 
-CREATE TABLE `role` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` varchar(300) NOT NULL,
-  `role_description` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role_description` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role_name`, `role_description`) VALUES
+(1, 'user', ''),
+(2, 'admin', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `themes`
+--
+
+CREATE TABLE IF NOT EXISTS `themes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) NOT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `themes`
+--
+
+INSERT INTO `themes` (`id`, `name`, `description`, `active`) VALUES
+(1, 'Energie', NULL, 1),
+(2, 'Afval', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -106,21 +172,42 @@ CREATE TABLE `role` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(300) NOT NULL,
   `last_name` varchar(300) NOT NULL,
   `email` varchar(300) NOT NULL,
   `password` varchar(300) NOT NULL,
-  `street` varchar(300) NOT NULL,
-  `house_number` varchar(300) NOT NULL,
-  `zip` varchar(4) NOT NULL,
-  `phone_number` varchar(300) NOT NULL,
   `avatar` varchar(300) DEFAULT 'default.jpg',
   `green_points` int(11) NOT NULL,
-  `role_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL DEFAULT '1',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `verified` tinyint(1) NOT NULL
+  `verified` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `avatar`, `green_points`, `role_id`, `created_at`, `verified`) VALUES
+(1, 'John', 'Doe', 'jdoe@test.com', '12345', 'default.jpg', 20, 1, '2020-05-05 14:25:59', 1),
+(2, 'Jana', 'Doe', '', '', 'default.jpg', 0, 0, '2020-05-05 14:40:22', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_information`
+--
+
+CREATE TABLE IF NOT EXISTS `user_information` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `street` varchar(300) NOT NULL,
+  `house_number` varchar(4) NOT NULL,
+  `zip` varchar(4) NOT NULL,
+  `phone_number` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -129,103 +216,12 @@ CREATE TABLE `users` (
 -- Table structure for table `verification_code`
 --
 
-CREATE TABLE `verification_code` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `verification_code` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `verify_code` varchar(300) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `challenges`
---
-ALTER TABLE `challenges`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `challenge_battles`
---
-ALTER TABLE `challenge_battles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `challenge_participators`
---
-ALTER TABLE `challenge_participators`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `cities`
---
-ALTER TABLE `cities`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `verification_code`
---
-ALTER TABLE `verification_code`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `challenges`
---
-ALTER TABLE `challenges`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `challenge_battles`
---
-ALTER TABLE `challenge_battles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `challenge_participators`
---
-ALTER TABLE `challenge_participators`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `cities`
---
-ALTER TABLE `cities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `verification_code`
---
-ALTER TABLE `verification_code`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
