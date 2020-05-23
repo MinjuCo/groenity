3b1d83ac5e132e9beaee4d063e45ef0a0cd768a5
brovnie <maarl.deenis@gmail.com>
2020-05-24 01:47:47 +0200

    layout city

---
+++ includes/cityMilieuImpact.inc.php
@@ -0,0 +1,54 @@
+  <div class="row">
+      <div class="card col-md-10 shadow-sm d-flex">
+          <div class="card-body mt-0">
+              <div class="row  d-flex justify-content-between">
+                  <div class="col-6 align-self-center">
+                      <h3>Milieu-impact</h3>
+                  </div>
+                  <div class="col-m-7 align-self-center">
+                      <input type="submit" value="Volg real-time op" class="btn form-control ">
+                  </div>
+
+              </div>
+              <div class="row">
+                  <ul class="nav col-md-12 nav-pills mb-1" id="pills-tab-milieu" role="tablist">
+                      <li class="nav-item ">
+                          <a class="nav-link active rounded-pill" id="pills-impact-enetgie-tab" data-toggle="pill" href="#pills-impact-energie" role="tab" aria-controls="pills-milieu" aria-selected="true">Energie</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link rounded-pill" id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false">Water</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link rounded-pill" id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false">Geluidsniveau</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link rounded-pill" id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false">Afval</a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="row">
+                  <ul class="nav  mb-1" id="pills-tab-milieu" role="tablist" >
+                      <li class="nav-item ">
+                          <a class="nav-link active border-bottom"  id="pills-impact-enetgie-tab" data-toggle="pill" href="#pills-impact-energie" role="tab" aria-controls="pills-milieu" aria-selected="true" style="color:#05386B;">GEVOLGEN</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link " id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false" style="color:#05386B;">TIPS</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link  " id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false" style="color:#05386B;">UITDAGINGEN</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link " id="pills-explore-tab" data-toggle="pill" href="#pills-explore" role="tab" aria-controls="pills-explore" aria-selected="false" style="color:#05386B;"><u></u></a>
+                      </li>
+                  </u>
+              </div>
+
+              <ul class="mt-3">
+                  <li><h5 class="ml-2">  De Aarde zal sneller opwarmen dan normaal</h5> </li>
+                  <li><h5 class="ml-2">  De Aarde zal sneller opwarmen dan normaal</h5> </li>
+                  <li><h5 class="ml-2">  De Aarde zal sneller opwarmen dan normaal</h5> </li>
+              </ul>
+          </div>
+      </div>
+  </div>
+  </div>
