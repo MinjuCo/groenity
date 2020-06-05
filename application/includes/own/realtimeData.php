f9fe91f3878696baa097c4863a912fee032fc318
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-06-05 17:18:16 +0200

    Block layout is ready. Content is missing.

---
+++ application/includes/own/realtimeData.php
@@ -0,0 +1,68 @@
+<div class="card shadow">
+  <div class="card-body">
+    <h3 class="card-title">Real-time gegevens</h3>
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+      <li class="nav-item">
+          <a class="nav-link active rounded-pill" id="pills-energy-tab" data-toggle="pill" href="#pills-energy" role="tab" aria-controls="pills-energy" aria-selected="true">Energie</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link rounded-pill" id="pills-water-tab" data-toggle="pill" href="#pills-water" role="tab" aria-controls="pills-water" aria-selected="false">Water</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link rounded-pill disabled" id="pills-soundlevel-tab" data-toggle="pill" href="#pills-level" role="tab" aria-controls="pills-soundlevel" aria-selected="false">Geluidsniveau</a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link rounded-pill disabled" id="pills-waste-tab" data-toggle="pill" href="#pills-waste" role="tab" aria-controls="pills-waste" aria-selected="false">Afval</a>
+      </li>
+    </ul>
+    <div class="tab-content" id="themeContent">
+      <div class="tab-pane fade show active" id="pills-energy" role="tabpanel" aria-labelledby="pills-energy-tab">
+        <ul class="nav nav-pills categories mb-4" id="pills-tab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="pills-daily-tab" data-toggle="pill" href="#pills-daily" role="tab" aria-controls="pills-daily" aria-selected="true">Dagelijks</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="pills-weekly-tab" data-toggle="pill" href="#pills-weekly" role="tab" aria-controls="pills-weekly" aria-selected="false">Wekelijks</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="pills-monthly-tab" data-toggle="pill" href="#pills-monthly" role="tab" aria-controls="pills-monthly" aria-selected="false">Maandelijks</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="pills-yearly-tab" data-toggle="pill" href="#pills-yearly" role="tab" aria-controls="pills-yearly" aria-selected="false">Jaarlijks</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="timeContent">
+          <div class="tab-pane fade show active" id="pills-daily" role="tabpanel" aria-labelledby="pills-daily-tab">
+            <h4>Dagelijks energieverbruik</h4>
+            <div>(grafiek)</div>
+          </div>
+          <div class="tab-pane fade" id="pills-weekly" role="tabpanel" aria-labelledby="pills-weekly-tab">
+            <h4>Wekelijks energieverbruik</h4>
+          </div>
+          <div class="tab-pane fade" id="pills-monthly" role="tabpanel" aria-labelledby="pills-monthly-tab">
+            <h4>Maandelijks energieverbruik</h4>
+          </div>
+          <div class="tab-pane fade" id="pills-yearly" role="tabpanel" aria-labelledby="pills-yearly-tab">
+            <h4>Jaarlijks energieverbruik</h4>
+          </div>
+        </div>
+        <h4>Limiet instellen</h4>
+        <p>Wanneer je een limiet insteld verschijnt er een rode lijn op de hoogte van je ingestelde maximum verbruik. 
+          Indien er kans is dat je verbruik bijna de maximum grens bereikt, zal je een notificatie krijgen.</p>
+        <form action="" method="post">
+          <div class="form-group">
+            <label for="maxLimit">Maximum verbruik</label>
+            <div class="input-group mb-3">
+              <input type="number" class="form-control" placeholder="Geef het maximum verbruik in waarbij je verwittigd wilt worden" id="maxLimit" name="maxLimit">
+              <div class="input-group-append">
+                <input type="submit" class="btn btn-outline-secondary" value="Stel verbruik in" name="submitLimitEnergy">
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="tab-pane fade" id="pills-water" role="tabpanel" aria-labelledby="pills-water-tab">...</div>
+      <div class="tab-pane fade" id="pills-soundlevel" role="tabpanel" aria-labelledby="pills-soundlevel-tab">...</div>
+    </div>
+  </div>
+</div>
--- application/own.php
+++ application/own.php
@@ -7,6 +7,12 @@
     $_SESSION['user'] = "Test";
     $userId = 1;
 
+    if(isset($_GET['p']) && !empty($_GET['p'])){
+      $requestedContent = htmlspecialchars($_GET['p']);
+    }else{
+      $requestedContent = "realtime";
+    }
+
     try{
       //$themes = Theme::getAllThemes();
       $completedChallenges = Challenge::getUserCompletedChallenge($userId); //Challenges that user already completed
@@ -29,69 +35,48 @@
         <h2 class="mb-4">Eigen gegevens</h2>
         <div class="row mb-3">
           <div class="col-md-8">
-            <div class="card shadow">
-              <div class="card-body">
-                <h3 class="card-title">Real-time gegevens</h3>
-                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                  <li class="nav-item">
-                      <a class="nav-link active rounded-pill" id="pills-energy-tab" data-toggle="pill" href="#pills-energy" role="tab" aria-controls="pills-energy" aria-selected="true">Energie</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link rounded-pill" id="pills-water-tab" data-toggle="pill" href="#pills-water" role="tab" aria-controls="pills-water" aria-selected="false">Water</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link rounded-pill disabled" id="pills-soundlevel-tab" data-toggle="pill" href="#pills-level" role="tab" aria-controls="pills-soundlevel" aria-selected="false">Geluidsniveau</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link rounded-pill disabled" id="pills-waste-tab" data-toggle="pill" href="#pills-waste" role="tab" aria-controls="pills-waste" aria-selected="false">Afval</a>
-                  </li>
-                </ul>
-                <div class="tab-content" id="themeContent">
-                  <div class="tab-pane fade show active" id="pills-energy" role="tabpanel" aria-labelledby="pills-energy-tab">
-                    <?php include_once(__DIR__."/includes/own/energyCategory.php"); ?>
-                  </div>
-                  <div class="tab-pane fade" id="pills-water" role="tabpanel" aria-labelledby="pills-water-tab">...</div>
-                  <div class="tab-pane fade" id="pills-soundlevel" role="tabpanel" aria-labelledby="pills-soundlevel-tab">...</div>
-                </div>
-              </div>
-            </div>
+            <?php 
+              if($requestedContent == "impact"):
+              
+                include_once(__DIR__.'/includes/own/environmentImpact.php');
+                
+              else:
+              
+                include_once(__DIR__.'/includes/own/realtimeData.php');
+                
+              endif; 
+            ?>
           </div>
           <div class="col-md-4">
-            <div class="row mb-4">
-              <div class="card col-12 shadow">
-                <div class="card-body">
-                  <h3 class="card-title">Profiel</h3>
-                  <div class="media">
-                    <img class="mr-3 avatar rounded-circle" src="../img/default.png" alt="Generic placeholder image">
-                    <div class="media-body">
-                      <h5 class="mt-0">[Naam gebruiker]</h5>
-                      [locatie]
-                    </div>
+            <div class="card shadow">
+              <div class="card-body">
+                <h3 class="card-title">Profiel</h3>
+                <div class="media">
+                  <img class="mr-3 avatar rounded-circle" src="../img/default.png" alt="Generic placeholder image">
+                  <div class="media-body">
+                    <h5 class="mt-0">[Naam gebruiker]</h5>
+                    [locatie]
                   </div>
                 </div>
               </div>
             </div>
-            <div class="row mb-4">
-              <ul class="nav nav-pills p-0 col-12" id="pills-tab" role="tablist">
-                <li class="nav-item w-100">
-                    <a class="nav-link btn btn-block active" id="pills-impact-tab" data-toggle="pill" href="#pills-impact" role="tab" aria-controls="pills-impact" aria-selected="true">Bekijk milieu-impact</a>
-                </li>
-              </ul>
-            </div>
-            <div class="row">
-              <div class="card col-12 shadow">
-                <div class="card-body">
-                  <h3 class="card-title">Prestaties</h3>
-                  <div class="media border mb-3 rounded text-center d-flex align-items-center">
-                    <img class="mr-3 avatar rounded-circle" src="../img/default.png" alt="Icon">
-                    <div class="media-body">
-                      Voltooide uitdagingen
-                      <h5>10</h5>
-                    </div>
+            <?php if($requestedContent == "impact"):?>
+              <a href="own.php" class="btn btn-block my-4">Volg real-time op</a>
+            <?php else:?>
+              <a href="?p=impact" class="btn btn-block my-4">Bekijk milieu-impact</a>
+            <?php endif;?>
+            <div class="card shadow">
+              <div class="card-body">
+                <h3 class="card-title">Prestaties</h3>
+                <div class="media border mb-3 rounded text-center d-flex align-items-center">
+                  <img class="mr-3 avatar rounded-circle" src="../img/default.png" alt="Icon">
+                  <div class="media-body">
+                    Voltooide uitdagingen
+                    <h5>10</h5>
                   </div>
-                  <a href="#meerPrestaties" class="btn btn-block btn-secondary">Toon meer prestaties</a>
-                  <a href="#voetafdruk" class="btn btn-block">Bereken voetafdruk</a>
                 </div>
+                <a href="#meerPrestaties" class="btn btn-block btn-secondary">Toon meer prestaties</a>
+                <a href="#voetafdruk" class="btn btn-block">Bereken voetafdruk</a>
               </div>
             </div>
           </div>
